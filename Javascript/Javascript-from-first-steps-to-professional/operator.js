542c9968ce60a2341a202214f78bdb185cea591e
Mike Pham <mikep@xello.world>
2024-03-31 03:41:45 -0400

    operator exercises done

---
+++ Javascript/Javascript-from-first-steps-to-professional/operator.js
@@ -0,0 +1,12 @@
+// code here is tested in browser https://imgur.com/a/817zJ4J
+// 1. total number of siblings of your parents
+const momSiblingsNum = 4;
+const dadSiblingsNum = 4;
+momSiblingsNum + dadSiblingsNum;
+
+// 2.average sleeping hour
+(5 + 6 + 5 + 4 + 5 + 5 + 4) / 7
+
+// 3. number of dogs you'd pet in a week if you pet 1 dog per hour while awake
+const sleepAverage = (5 + 6 + 5 + 4 + 5 + 5 + 4) / 7;
+24/ sleepAverage * 7
