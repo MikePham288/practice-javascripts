8fa0a804946b0db0efe66e2a488d2230812df009
Mike Pham <mikep@xello.world>
2024-03-31 03:19:28 -0400

    finish string exercises

---
+++ Javascript/Javascript-from-first-steps-to-professional/string.js
@@ -0,0 +1,12 @@
+// code here is tested in browser https://imgur.com/a/Go9b0NH
+// 1. Add your last name in the players listing
+document.getElementById('p1-name').append(" Pham");
+
+// 2. Retrieve the first "T" in the page title
+document.title.indexOf('T');
+
+// Answer whether the page title contains the string Javascript
+document.title.includes("JavaScript");
+
+// Capitalize the heading "Tic Tac Toe"
+document.querySelector('header h1').style.textTransform = 'uppercase';
